a91a27208ba0f9e49f8f3d75120d1d42f7e329f0
Dennis Iversen <deiv@aarhus.dk>
2024-04-02 11:23:02 +0200

    add files

---
+++ README.md
@@ -0,0 +1,25 @@
+# barcode-scanner
+
+This is a simple pwa barcode scanner. 
+
+It does not need internet to work. For installing and updating it you need to have internet.
+
+It uses this library for the scanning: https://github.com/mebjas/html5-qrcode
+
+The app will load the data from `data.json`.
+
+When scanning it will look up results found in the data.json file. 
+
+If a result is found it will display the result - otherwise it will display the value of the scanned barcode.
+
+# Updating
+
+Change the `data.json` file and push the changes to the server. Alter the `CACHE_NAME`  in the service worker to force the update.
+Alter this line:
+
+    const CACHE_NAME = 'qr-scanner-v1';
+
+# Assets
+
+Assets and icons are generated using https://favicon.io/
+
---
+++ service-worker.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'qr-scanner-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/html5-qrcode.min.js',
+  '/html5-qrcode-demo.js',
+  '/data.json',
+  '/favicon_io/android-chrome-192x192.png',
+  '/favicon_io/android-chrome-512x512.png',
+  '/favicon_io/apple-touch-icon.png',
+  '/favicon_io/favicon-32x32.png',
+  '/favicon_io/favicon-16x16.png',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Caching URLs:', urlsToCache);
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(
+          response => {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            var responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          }
+        );
+      })
+    );
+});
