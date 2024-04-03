c6cffd7cbf9e5a69cbf960c2b8454b00e0303713
Dennis Iversen <deiv@aarhus.dk>
2024-04-03 11:43:09 +0200

    add activate function

--- service-worker.js
+++ service-worker.js
@@ -21,6 +21,21 @@ self.addEventListener('install', event => {
   );
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => {
+          return cacheName !== CACHE_NAME;
+        }).map(cacheName => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
@@ -32,7 +47,7 @@ self.addEventListener('fetch', event => {
         return fetch(event.request).then(
           response => {
             // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
               return response;
             }
             var responseToCache = response.clone();
@@ -44,5 +59,5 @@ self.addEventListener('fetch', event => {
           }
         );
       })
-    );
+  );
 });
