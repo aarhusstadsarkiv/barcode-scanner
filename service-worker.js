1cb1ee060654d3f352f94bf2bb16ef0f0ed6b3a4
Dennis Iversen <deiv@aarhus.dk>
2024-04-03 14:17:54 +0200

    try to bust cache

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'qr-scanner-v1.2.8';
+const CACHE_NAME = 'v1.2.7';
 const urlsToCache = [
   '/barcode-scanner/',
   '/barcode-scanner/index.html',
   '/barcode-scanner/html5-qrcode.min.js',
-  `/barcode-scanner/data.json`,
+  `/barcode-scanner/data.json?v=${CACHE_NAME}`,
   '/barcode-scanner/favicon_io/android-chrome-192x192.png',
   '/barcode-scanner/favicon_io/android-chrome-512x512.png',
   '/barcode-scanner/favicon_io/apple-touch-icon.png',
@@ -37,24 +37,28 @@ self.addEventListener('activate', event => {
   );
 });
 
-// Try to get the resource from the network
-// before falling back to the cache
 self.addEventListener('fetch', event => {
   event.respondWith(
-    fetch(event.request).then(networkResponse => {
-      caches.open(CACHE_NAME).then(cache => {
-        cache.put(event.request, networkResponse.clone());
-      });
-      // And return the network response
-      return networkResponse;
-    }).catch(() => {
-      // If the network request fails, try to serve the resource from the cache
-      return caches.match(event.request).then(response => {
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
         if (response) {
           return response;
         }
-        // Optionally, provide a fallback
-      });
-    })
-  );
+        return fetch(event.request).then(
+          response => {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            var responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          }
+        );
+      })
+    );
 });
