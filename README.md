4a1c1dae11a703827f1d31b59343210558ec53a8
Dennis Iversen <deiv@aarhus.dk>
2024-04-04 10:22:37 +0200

    clean up source. Refactor a bit.

--- README.md
+++ README.md
@@ -1,34 +1,38 @@
 # barcode-scanner
 
-This is a simple pwa barcode scanner. 
-
-It does not need internet to work. For installing and updating it you need to have internet.
-
-It uses this library for the scanning: https://github.com/mebjas/html5-qrcode
+This is a simple PWA barcode scanner. It does not need internet to work. 
+For installing and updating it you need to have internet.
 
 The app will load the data from `data.json`.
 
 When scanning it will look up results found in the data.json file. 
 
-If a result is found it will display the result - otherwise it will display the value of the scanned barcode.
+If a result is found it will display the result - 
+otherwise it will display the value of the scanned barcode.
 
 # Install
 
-Go to https://aarhusstadsarkiv.github.io/barcode-scanner/ and in the browser main menu choose "Install app".
+Go to https://acceptable-turkey.surge.sh 
 
-Then you can place the app icon on your phone's home screen.
+You may be prompted for installing it. Then accept. If not prompted
+then install it using the browser menu (`Install app`).
+Then you can place the app icon on your phone's home screen. 
 
 # Updating
 
-Change the `data.json` file and push the changes to the server. Alter the `CACHE_NAME` in the service worker to force the update.
-Alter this line:
+Change the `data.json` file. Alter the `VERSION` in the 
+service worker to force the update. Set the `VERSION` in `index.html`
 
-    const CACHE_NAME = 'qr-scanner-v1';
+Then push to remove server: 
+
+    surge . acceptable-turkey.surge.sh
+
+If there is a new version the phone will update the app. Then the new app will be 
+activated on the next restart of the app. You can force a install by 
+clicking on the app icon on the desktop and press `Site settings` and then press
+`Delete data & reset permissions`. Then the app will is forced to load the latest version.  
 
 # Assets
 
 Assets and icons are generated using https://favicon.io/
-
-# Sever
-
-surge . acceptable-turkey.surge.sh
+It uses this library for the scanning: https://github.com/mebjas/html5-qrcode
--- service-worker.js
+++ service-worker.js
@@ -1,64 +1,68 @@
-const CACHE_NAME = 'v1.4.0';
+const VERSION = 'v2.0.1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/html5-qrcode.min.js',
-  `/data.json`,
-  '/favicon_io/android-chrome-192x192.png',
-  '/favicon_io/android-chrome-512x512.png',
-  '/favicon_io/apple-touch-icon.png',
-  '/favicon_io/favicon-32x32.png',
-  '/favicon_io/favicon-16x16.png',
+	'/',
+	'/index.html',
+	'/html5-qrcode.min.js',
+	`/data.json`,
+	'/favicon_io/android-chrome-192x192.png',
+	'/favicon_io/android-chrome-512x512.png',
+	'/favicon_io/apple-touch-icon.png',
+	'/favicon_io/favicon-32x32.png',
+	'/favicon_io/favicon-16x16.png',
+	'/manifest.json',
 ];
 
-console.log('cache name', CACHE_NAME);
+console.log('cache name', VERSION);
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Caching URLs:', urlsToCache);
-        return cache.addAll(urlsToCache);
-      })
-  );
+	console.log('Installing new service worker...');
+	event.waitUntil(
+		caches.open(VERSION)
+			.then(cache => {
+				return cache.addAll(urlsToCache);
+			})
+	);
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.filter(cacheName => {
-          return cacheName !== CACHE_NAME;
-        }).map(cacheName => {
-          return caches.delete(cacheName);
-        })
-      );
-    })
-  );
+	console.log('Activating new service worker...');
+	event.waitUntil(
+		caches.keys().then(cacheNames => {
+			console.log('Current caches:', cacheNames);
+			return Promise.all(
+				cacheNames.filter(cacheName => {
+					const isOldCache = cacheName !== VERSION;
+					console.log(`Checking cache: ${cacheName}, delete: ${isOldCache}`);
+					return isOldCache;
+				}).map(cacheName => {
+					console.log(`Deleting cache: ${cacheName}`);
+					return caches.delete(cacheName);
+				})
+			);
+		})
+	);
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request).then(
-          response => {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-            var responseToCache = response.clone();
-            caches.open(CACHE_NAME)
-              .then(cache => {
-                cache.put(event.request, responseToCache);
-              });
-            return response;
-          }
-        );
-      })
-    );
+	event.respondWith(
+		caches.match(event.request).then(response => {
+			// Cache hit - return response
+			if (response) {
+				return response;
+			}
+			return fetch(event.request).then(
+				response => {
+					// Check if we received a valid response
+					if (!response || response.status !== 200 || response.type !== 'basic') {
+						return response;
+					}
+					var responseToCache = response.clone();
+					caches.open(VERSION).then(cache => {
+						cache.put(event.request, responseToCache);
+					});
+					return response;
+				}
+			);
+		})
+	);
 });
